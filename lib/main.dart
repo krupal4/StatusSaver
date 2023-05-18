868ed2d49050216109b29c0f90386b32a316b4c6
Rishabh11200 <rishabhshah1100@gmail.com>
2023-05-18 11:26:04 +0530

    Added Dynmic colors fetched from wallpaper

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:status_saver/common.dart';
@@ -5,6 +6,7 @@ import 'package:status_saver/provider/locale_provider.dart';
 import 'package:status_saver/provider/storage_permission_provider.dart';
 import 'package:status_saver/provider/theme_provider.dart';
 import 'package:status_saver/screens/home_screen.dart';
+import 'package:status_saver/theme/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,39 +19,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<LocaleProvider>(create: (context) => LocaleProvider()..initialize()),
-        ChangeNotifierProvider<ThemeModeProvider>(create: (context) => ThemeModeProvider()..initialize()),
-        ChangeNotifierProvider<StoragePermissionProvider>(create: (context) => StoragePermissionProvider()..initialize())
-      ],
-      builder: (context,child) {
-        final localeProvider = Provider.of<LocaleProvider>(context);
-        final themeModeProvider = Provider.of<ThemeModeProvider>(context);
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Whatsapp Status Saver',
-          localizationsDelegates: const [
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-            AppLocalizations.delegate
-          ],
-          locale: localeProvider.locale,
-          supportedLocales: AppLocalizations.supportedLocales,
-          themeMode: themeModeProvider.themeMode,
-          theme: ThemeData(
-            useMaterial3: true,
-            brightness: Brightness.light,
-            colorSchemeSeed: Colors.green[700],
-          ),
-          darkTheme: ThemeData(
-            useMaterial3: true,
-            brightness: Brightness.dark,
-            colorSchemeSeed: Colors.green[700],
-          ),
-          home: const HomeScreen(),
-        );
-      }
-    );
+        providers: [
+          ChangeNotifierProvider<LocaleProvider>(
+              create: (context) => LocaleProvider()..initialize()),
+          ChangeNotifierProvider<ThemeModeProvider>(
+              create: (context) => ThemeModeProvider()..initialize()),
+          ChangeNotifierProvider<StoragePermissionProvider>(
+              create: (context) => StoragePermissionProvider()..initialize())
+        ],
+        builder: (context, child) {
+          final localeProvider = Provider.of<LocaleProvider>(context);
+          final themeModeProvider = Provider.of<ThemeModeProvider>(context);
+          return DynamicColorBuilder(builder:
+              (ColorScheme? lightColorScheme, ColorScheme? darkColorScheme) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Whatsapp Status Saver',
+              localizationsDelegates: const [
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+                AppLocalizations.delegate
+              ],
+              locale: localeProvider.locale,
+              supportedLocales: AppLocalizations.supportedLocales,
+              themeMode: themeModeProvider.themeMode,
+              theme: AppTheme.lightTheme(lightColorScheme),
+              darkTheme: AppTheme.darkTheme(darkColorScheme),
+              home: const HomeScreen(),
+            );
+          });
+        });
   }
 }
---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData lightTheme(ColorScheme? lightColorScheme) {
+    ColorScheme scheme = lightColorScheme ??
+        ColorScheme.fromSeed(seedColor: const Color(0x00FF00));
+    return ThemeData(
+      useMaterial3: true,
+      colorScheme: scheme,
+    );
+  }
+
+  static ThemeData darkTheme(ColorScheme? darkColorScheme) {
+    ColorScheme scheme = darkColorScheme ??
+        ColorScheme.fromSeed(
+            seedColor: const Color(0x00FF00), brightness: Brightness.dark);
+    return ThemeData(
+      useMaterial3: true,
+      colorScheme: scheme,
+    );
+  }
+}
